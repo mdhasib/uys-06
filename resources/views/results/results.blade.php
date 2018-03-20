be62d0e0793e31e1c73e59da4ccfd41b2dd63f03
unknown <rhasibur@hotmail.com>
2018-03-20 10:54:23 +0600

    first commit

---
+++ resources/views/results/results.blade.php
@@ -0,0 +1,155 @@
+@extends('layouts.master')
+@section('title', 'MATCH RESULT , RUSSIA 18')
+@section('content')
+    <!-- page title & breadcrumbs start -->
+    <div class="bdcmb-bg2 page-head parallax overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h3>LA LIGA RESULT</h3>
+                    </div>
+                </div>
+                <!-- /.colour-service-1-->
+                <div class="col-md-12">
+                    <ol class="breadcrumb">
+                        <li><a href="index.html">home</a></li>
+                        <li>।</li>
+                        <li>club list</li>
+                        <li>।</li>
+                        <li>barcelona</li>
+                    </ol>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.page-header -->
+    <!-- page title & breadcrumbs end -->
+
+    <!--result area start-->
+    <div class="laliga-box result-box pad60">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="text-center">la liga</h4>
+                    <div class="fixture-box bb-1 ">
+                        <div class="club-fixture">
+                            <div class="club-img">
+                                <img class="sm-img" src="assets/images/club-logo/logo/barcelona.png" alt="club logo">
+                                <span>barcelona</span>
+                            </div>
+                            <div class="club-schedule text-center">
+                                <h5>2 : 2</h5>
+                                <p>2018-june-20 12.00 am</p>
+                            </div>
+                            <div class="club-img">
+                                <span>real madrid</span>
+                                <img class="sm-img" src="assets/images/club-logo/logo/realmadrid.png" alt="club logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fixture-box bb-1">
+                        <div class="club-fixture  active">
+                            <div class="club-img">
+                                <img class="sm-img" src="assets/images/club-logo/logo/atletico.png" alt="club logo">
+                                <span>Atlético Madrid</span>
+                            </div>
+                            <div class="club-schedule text-center">
+                                <h5>2 : 1</h5>
+                                <p>2018-june-20 12.00 am</p>
+                            </div>
+                            <div class="club-img">
+                                <span>villarreal</span>
+                                <img class="sm-img" src="assets/images/club-logo/logo/villarreal.png" alt="club logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fixture-box bb-1">
+                        <div class="club-fixture">
+                            <div class="club-img">
+                                <img class="sm-img" src="assets/images/club-logo/logo/sevilla.png" alt="club logo">
+                                <span>sevilla</span>
+                            </div>
+                            <div class="club-schedule text-center">
+                                <h5>0 : 1</h5>
+                                <p>2018-june-20 12.00 am</p>
+                            </div>
+                            <div class="club-img">
+                                <span>real betis</span>
+                                <img class="sm-img" src="assets/images/club-logo/logo/realbetis.png" alt="club logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fixture-box bb-1">
+                        <div class="club-fixture">
+                            <div class="club-img">
+                                <img class="sm-img" src="assets/images/club-logo/logo/alves.png" alt="club logo">
+                                <span>alves</span>
+                            </div>
+                            <div class="club-schedule text-center">
+                                <h5>1 : 4</h5>
+                                <p>2018-june-20 12.00 am</p>
+                            </div>
+                            <div class="club-img">
+                                <span>barcelona</span>
+                                <img class="sm-img" src="assets/images/club-logo/logo/barcelona.png" alt="club logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fixture-box bb-1">
+                        <div class="club-fixture">
+                            <div class="club-img">
+                                <img class="sm-img" src="assets/images/club-logo/logo/realmadrid.png" alt="club logo">
+                                <span>real madrid</span>
+                            </div>
+                            <div class="club-schedule text-center">
+                                <h5>5 : 2</h5>
+                                <p>2018-june-20 12.00 am</p>
+                            </div>
+                            <div class="club-img">
+                                <span>malaga</span>
+                                <img class="sm-img" src="assets/images/club-logo/logo/malaga.png" alt="club logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fixture-box bb-1">
+                        <div class="club-fixture">
+                            <div class="club-img">
+                                <img class="sm-img" src="assets/images/club-logo/logo/barcelona.png" alt="club logo">
+                                <span>barcelona</span>
+                            </div>
+                            <div class="club-schedule text-center">
+                                <h5>3 : 2</h5>
+                                <p>2018-june-20 12.00 am</p>
+                            </div>
+                            <div class="club-img">
+                                <span>real madrid</span>
+                                <img class="sm-img" src="assets/images/club-logo/logo/realmadrid.png" alt="club logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fixture-box">
+                        <div class="club-fixture">
+                            <div class="club-img">
+                                <img class="sm-img" src="assets/images/club-logo/logo/villarreal.png" alt="club logo">
+                                <span>villarreal</span>
+                            </div>
+                            <div class="club-schedule text-center">
+                                <h5>1 : 2</h5>
+                                <p>2018-june-20 12.00 am</p>
+                            </div>
+                            <div class="club-img">
+                                <span>sevilla</span>
+                                <img class="sm-img" src="assets/images/club-logo/logo/sevilla.png" alt="club logo">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--fixtures area end-->
+@endsection
